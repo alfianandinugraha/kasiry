ee7903a1dca4128d41108b7a77dc1cf79b787126
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-12-29 00:39:25 +0700

    fix(frontend): fix rules minLength and maxLenght

--- frontend/app/src/main/java/com/kasiry/app/rules/maxLength.kt
+++ frontend/app/src/main/java/com/kasiry/app/rules/maxLength.kt
@@ -6,7 +6,7 @@ fun maxLength(length: Int): (Any) -> Either<String, Boolean> {
     return fun (value: Any): Either<String, Boolean> {
         return when (value)  {
             is String -> {
-                if (value.length < length) {
+                if (value.length > length) {
                     Either.Left("This field must be at least $length characters")
                 } else {
                     Either.Right(true)
--- frontend/app/src/main/java/com/kasiry/app/rules/minLength.kt
+++ frontend/app/src/main/java/com/kasiry/app/rules/minLength.kt
@@ -6,7 +6,7 @@ fun minLength(length: Int): (Any) -> Either<String, Boolean> {
     return fun (value: Any): Either<String, Boolean> {
         return when (value)  {
             is String -> {
-                if (value.length > length) {
+                if (value.length < length) {
                     Either.Left("This field must be at most $length characters")
                 } else {
                     Either.Right(true)
