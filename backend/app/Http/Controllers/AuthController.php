e589820b2baf34c07148359bd2e48c4c4119a603
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-12-30 20:40:36 +0700

    feat(backend): create feature auth

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Restriction;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "email" => ["required", "email", "exists:users,email"],
+            "password" => ["required"],
+        ]);
+
+        $validator->validate();
+
+        $user = User::where("email", $request->email)->firstOrFail();
+        $isValid = Hash::check($request->password, $user->password);
+
+        if (!$isValid) {
+            return Response::json(
+                [
+                    "message" => "Password dan email tidak sesuai",
+                ],
+                401
+            );
+        }
+
+        $expire = Carbon::now();
+        $expire->addDays(7);
+        $token = $user->createToken("access-token", [], $expire)
+            ->plainTextToken;
+
+        return Response::json([
+            "message" => "Login successful",
+            "data" => [
+                "token" => $token,
+            ],
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "name" => ["required", "string", "max:255"],
+            "phone" => ["required", "string", "max:255"],
+            "email" => ["required", "email", "unique:users,email"],
+            "password" => ["required", "min:8"],
+        ]);
+
+        $validator->validate();
+
+        $restrictionId = Str::random(10);
+        $restriction = new Restriction([
+            "restriction_id" => $restrictionId,
+            "allowed" => [Restriction::SUPER],
+        ]);
+
+        $userId = Str::random(10);
+        $user = new User([
+            "user_id" => $userId,
+            "name" => $request->name,
+            "phone" => $request->phone,
+            "email" => $request->email,
+            "password" => Hash::make($request->password),
+            "restriction_id" => $restrictionId,
+        ]);
+
+        DB::transaction(function () use ($user, $restriction) {
+            $restriction->saveOrFail();
+            $user->saveOrFail();
+        });
+
+        return Response::json([
+            "message" => "Registration successful",
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request
+            ->user()
+            ->currentAccessToken()
+            ->delete();
+
+        return Response::json([
+            "message" => "Logout successful",
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
     return $request->user();
+});
+
+Route::post("/login", [AuthController::class, "login"]);
+Route::post("/register", [AuthController::class, "register"]);
+
+Route::middleware("auth:sanctum")->group(function () {
+    Route::post("/logout", [AuthController::class, "logout"]);
 });
