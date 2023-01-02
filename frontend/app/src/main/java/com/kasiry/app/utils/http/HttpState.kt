de52e71726b47cbc7f87fc8e65853ef6e356f637
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-01-02 23:12:37 +0700

    feat(frontend): add errors in HttpState.Error and copy method on success

--- frontend/app/src/main/java/com/kasiry/app/utils/http/HttpState.kt
+++ frontend/app/src/main/java/com/kasiry/app/utils/http/HttpState.kt
@@ -5,13 +5,18 @@ sealed class HttpState<out S, out E>() {
         val data: S,
         val message: String? = "",
         val statusCode: Int = 200
-    ) : HttpState<S, Nothing>()
+    ) : HttpState<S, Nothing>() {
+        fun copy() = Success(data, message, statusCode)
+    }
 
     class Error<E>(
         val data: E? = null,
         val message: String? = "",
-        val statusCode: Int = 400
-    ) : HttpState<Nothing, E>()
+        val statusCode: Int = 400,
+        val errors: Map<String, List<String>>? = null
+    ) : HttpState<Nothing, E>() {
+        fun copy() = Error(data, message, statusCode, errors)
+    }
 
     class Loading<S, E> : HttpState<S, E>()
 }
