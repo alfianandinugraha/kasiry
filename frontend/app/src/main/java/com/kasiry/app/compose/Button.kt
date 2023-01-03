7e9cb73663b60d54572bece5828c08889ad4e0b3
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-01-03 22:06:55 +0700

    feat(frontend): add prop bgColor in Button

--- frontend/app/src/main/java/com/kasiry/app/compose/Button.kt
+++ frontend/app/src/main/java/com/kasiry/app/compose/Button.kt
@@ -13,17 +13,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import com.kasiry.app.theme.blue
 
 @Composable
-fun Button(modifier: Modifier = Modifier, onClick: (() -> Unit)? = null, disabled: Boolean = false, content: @Composable () -> Unit) {
+fun Button(
+    modifier: Modifier = Modifier,
+    onClick: (() -> Unit)? = null,
+    disabled: Boolean = false,
+    bgColor: (disabled: Boolean) -> Color = { Color.blue(if (disabled) 300 else 500) },
+    content: @Composable () -> Unit
+) {
     val ripple = rememberRipple(bounded = false)
     Row(
         modifier = modifier
             .clip(shape = RoundedCornerShape(6.dp))
-            .background(color = Color.blue(if (disabled) 300 else 500))
+            .background(color = bgColor(disabled))
             .padding(14.dp)
             .clickable(indication = ripple, interactionSource = remember { MutableInteractionSource() }, enabled = !disabled) {
                 if (onClick != null) {
