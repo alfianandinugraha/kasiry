18c7f4ca3869c99bb9f2e44762bbb0b7bbf12adf
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-01-03 18:01:48 +0700

    feat(frontend): create ProfileRepository

---
+++ frontend/app/src/main/java/com/kasiry/app/repositories/ProfileRepository.kt
@@ -0,0 +1,30 @@
+package com.kasiry.app.repositories
+
+import android.content.Context
+import com.kasiry.app.models.data.Profile
+import com.kasiry.app.utils.http.*
+import okhttp3.OkHttpClient
+
+class ProfileRepository(context: Context) {
+    private val client = OkHttpClient
+        .Builder()
+        .addInterceptor(
+            AccessTokenInterceptor(context)
+        )
+        .build()
+
+    suspend fun get(): HttpState<Profile> {
+        val request = ApiRequest
+            .json(
+                HttpMethod.GET,
+                "/profile",
+            )
+            .build()
+
+        return HttpRequest
+            .create<Profile>(
+                call = client.newCall(request),
+            )
+            .execute()
+    }
+}
