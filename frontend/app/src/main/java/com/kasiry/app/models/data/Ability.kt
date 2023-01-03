d7e73898ff92d03a561ef8e411a8460abea6b88e
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-01-04 01:08:38 +0700

    feat(frontend): create component Guard and model Ability

---
+++ frontend/app/src/main/java/com/kasiry/app/models/data/Ability.kt
@@ -0,0 +1,23 @@
+package com.kasiry.app.models.data
+
+class Ability {
+    companion object {
+        const val SUPER = "*"
+        const val ADD_CATEGORY = "add-category"
+        const val UPDATE_CATEGORY = "update-category"
+        const val DELETE_CATEGORY = "delete-category"
+
+        const val ADD_PRODUCT = "add-product"
+        const val UPDATE_PRODUCT = "update-product"
+        const val DELETE_PRODUCT = "delete-product"
+
+        const val ADD_EMPLOYEE = "add-employee"
+        const val UPDATE_EMPLOYEE = "update-employee"
+        const val DELETE_EMPLOYEE = "delete-employee"
+
+        const val ADD_TRANSACTION = "add-transaction"
+        const val DELETE_TRANSACTION = "delete-transaction"
+
+        const val UPDATE_COMPANY = "update-company"
+    }
+}
