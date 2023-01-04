b6dd62bbc199e7cbebcfb9cdc88746e138a3344c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-01-04 19:01:07 +0700

    feat(frontend): add error handling on CategoryUpdateScreen

--- frontend/app/src/main/java/com/kasiry/app/screen/CategoryUpdateScreen.kt
+++ frontend/app/src/main/java/com/kasiry/app/screen/CategoryUpdateScreen.kt
@@ -1,6 +1,7 @@
 package com.kasiry.app.screen
 
 import android.app.Application
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -13,7 +14,6 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material.icons.rounded.Label
-import androidx.compose.material.icons.rounded.Person
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,15 +23,9 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.kasiry.app.compose.Alert
-import com.kasiry.app.compose.Button
-import com.kasiry.app.compose.TextField
-import com.kasiry.app.compose.TopBar
-import com.kasiry.app.models.data.Ability
+import com.kasiry.app.compose.*
 import com.kasiry.app.models.data.Category
 import com.kasiry.app.repositories.CategoryRepository
-import com.kasiry.app.repositories.EmployeeRepository
-import com.kasiry.app.rules.minLength
 import com.kasiry.app.rules.required
 import com.kasiry.app.theme.Typo
 import com.kasiry.app.theme.blue
@@ -39,7 +33,6 @@ import com.kasiry.app.theme.red
 import com.kasiry.app.utils.Field
 import com.kasiry.app.utils.FormStore
 import com.kasiry.app.utils.http.HttpState
-import com.kasiry.app.viewmodel.CategoryCreateViewModel
 import com.kasiry.app.viewmodel.CategoryUpdateViewModel
 import com.kasiry.app.viewmodel.MainViewModel
 import org.koin.androidx.compose.get
@@ -54,10 +47,13 @@ fun CategoryUpdateScreen(
     val profile by mainViewModel.profile.collectAsState()
 
     val categoryRepository: CategoryRepository = get()
-    val viewModel = CategoryUpdateViewModel(
-        application = application,
-        categoryRepository = categoryRepository
-    )
+    val viewModel = remember {
+        CategoryUpdateViewModel(
+            application = application,
+            categoryRepository = categoryRepository
+        )
+    }
+    val category by viewModel.category.collectAsState()
 
     val form = remember {
         FormStore(
@@ -76,6 +72,10 @@ fun CategoryUpdateScreen(
         viewModel.get(categoryId) {
             onSuccess {
                 form.setValue("name", it.data.name)
+            }
+            onError {
+                Toast.makeText(application.applicationContext, "Gagal memuat kategory", Toast.LENGTH_SHORT).show()
+                navController.popBackStack()
             }
         }
     }
@@ -145,83 +145,101 @@ fun CategoryUpdateScreen(
                 navController.popBackStack()
             },
         )
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 32.dp)
-        ) {
-            Icon(
-                imageVector = Icons.Rounded.Label,
-                contentDescription = null,
-                tint = Color.White,
-                modifier = Modifier
-                    .clip(
-                        shape = RoundedCornerShape(999.dp)
+        when (val categoryState = category) {
+            is HttpState.Success -> {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 32.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Label,
+                        contentDescription = null,
+                        tint = Color.White,
+                        modifier = Modifier
+                            .clip(
+                                shape = RoundedCornerShape(999.dp)
+                            )
+                            .background(Color.blue())
+                            .padding(16.dp)
+                            .size(52.dp)
+                            .align(Alignment.CenterHorizontally)
                     )
-                    .background(Color.blue())
-                    .padding(16.dp)
-                    .size(52.dp)
-                    .align(Alignment.CenterHorizontally)
-            )
-            Text(
-                text = "Informasi Kategori",
-                style = Typo.body,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(top = 16.dp)
-            )
-            TextField(
-                label = "Nama",
-                control = form,
-                name = "name",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp, top = 8.dp),
-            )
-            when(val profileState = profile) {
-                is HttpState.Success -> {
-                    Button(
+                    Text(
+                        text = "Informasi Kategori",
+                        style = Typo.body,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(top = 16.dp)
+                    )
+                    TextField(
+                        label = "Nama",
+                        control = form,
+                        name = "name",
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(top = 16.dp),
-                        onClick = {
-                            form.handleSubmit {
-                                val name = it["name"]?.value as String
+                            .padding(bottom = 16.dp, top = 8.dp),
+                    )
+                    when(val profileState = profile) {
+                        is HttpState.Success -> {
+                            Button(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(top = 16.dp),
+                                onClick = {
+                                    form.handleSubmit {
+                                        val name = it["name"]?.value as String
 
-                                viewModel.update(
-                                    category = Category(
-                                        categoryId = categoryId,
-                                        name = name,
-                                        companyId = profileState.data.companyId
-                                    )
-                                ) {
-                                    onSuccess {
-                                        navController.popBackStack()
+                                        viewModel.update(
+                                            category = Category(
+                                                categoryId = categoryId,
+                                                name = name,
+                                                companyId = profileState.data.companyId
+                                            )
+                                        ) {
+                                            onSuccess {
+                                                navController.popBackStack()
+                                            }
+                                            onError {
+                                                Toast
+                                                    .makeText(
+                                                        application.applicationContext,
+                                                        "Gagal mengubah kategori",
+                                                        Toast.LENGTH_SHORT
+                                                    )
+                                                    .show()
+                                            }
+                                        }
                                     }
                                 }
+                            ) {
+                                Text(
+                                    text = "Simpan",
+                                    style = Typo.body,
+                                    color = Color.White,
+                                )
                             }
-                        }
-                    ) {
-                        Text(
-                            text = "Simpan",
-                            style = Typo.body,
-                            color = Color.White,
-                        )
-                    }
-                    Text(
-                        text = "Hapus",
-                        style = Typo.body,
-                        color = Color.red(),
-                        modifier = Modifier
-                            .padding(top = 14.dp)
-                            .align(Alignment.CenterHorizontally)
-                            .clickable {
-                                isAlertShow = true
-                            }
-                    )
+                            Text(
+                                text = "Hapus",
+                                style = Typo.body,
+                                color = Color.red(),
+                                modifier = Modifier
+                                    .padding(top = 14.dp)
+                                    .align(Alignment.CenterHorizontally)
+                                    .clickable {
+                                        isAlertShow = true
+                                    }
+                            )
 
+                        }
+                        else -> {}
+                    }
                 }
-                else -> {}
             }
+            is HttpState.Loading -> {
+                Loading()
+            }
+            else -> {}
         }
+
     }
 }
