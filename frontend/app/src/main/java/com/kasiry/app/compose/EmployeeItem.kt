419a80f7b2bf8878c17fa693acfd90d1381f8045
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-01-06 01:59:40 +0700

    fix(frontend): fix indication on EmployeeItem

--- frontend/app/src/main/java/com/kasiry/app/compose/EmployeeItem.kt
+++ frontend/app/src/main/java/com/kasiry/app/compose/EmployeeItem.kt
@@ -2,16 +2,20 @@ package com.kasiry.app.compose
 
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ChevronRight
 import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -30,15 +34,21 @@ fun EmployeeItem(
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .clip(
+                shape = RoundedCornerShape(8.dp)
+            )
+            .clickable(
+                indication = rememberRipple(bounded = false),
+                interactionSource = remember { MutableInteractionSource() }
+            ) {
+                onClick(employee)
+            }
             .border(
                 width = 1.dp,
                 color = Color.gray(300),
                 shape = RoundedCornerShape(8.dp)
             )
             .padding(16.dp)
-            .clickable {
-                onClick(employee)
-            }
     ) {
         Icon(
             Icons.Rounded.Person, contentDescription = null,
