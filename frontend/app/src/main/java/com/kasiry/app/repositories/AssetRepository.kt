4875b9976fac141112633799b861f679084c4b0d
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-01-07 00:27:36 +0700

    feat(frontend): create AssetRepository

---
+++ frontend/app/src/main/java/com/kasiry/app/repositories/AssetRepository.kt
@@ -0,0 +1,56 @@
+package com.kasiry.app.repositories
+
+import android.content.Context
+import com.kasiry.app.models.data.Asset
+import com.kasiry.app.utils.http.*
+import okhttp3.MultipartBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.File
+import java.io.InputStream
+
+class AssetRepository(
+    context: Context
+) {
+    private val client = OkHttpClient
+        .Builder()
+        .addInterceptor(
+            AccessTokenInterceptor(context)
+        )
+        .build()
+
+    data class UploadBody(
+        val fileName: String,
+        val file: InputStream,
+        val companyId: String
+    )
+
+    suspend fun upload(body: UploadBody): HttpState<Asset> {
+        val multipartBody = MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+            .addFormDataPart("fileName", body.fileName)
+            .addFormDataPart(
+                "file",
+                body.fileName,
+                body.file.readBytes().toRequestBody()
+            )
+            .addFormDataPart("companyId", body.companyId)
+            .build()
+
+        val request = ApiRequest
+            .formData(
+                HttpMethod.POST,
+                "/assets",
+                multipartBody
+            )
+            .build()
+
+        return HttpRequest
+            .create<Asset>(
+                call = client.newCall(request)
+            )
+            .execute()
+    }
+}
