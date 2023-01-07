b3d5d05e516ab074ea7788ab58ed20285b96be02
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-01-07 14:09:16 +0700

    feat(frontend): create ProductRepository

---
+++ frontend/app/src/main/java/com/kasiry/app/repositories/ProductRepository.kt
@@ -0,0 +1,119 @@
+package com.kasiry.app.repositories
+
+import android.content.Context
+import com.kasiry.app.models.data.Product
+import com.kasiry.app.utils.http.*
+import okhttp3.OkHttpClient
+
+class ProductRepository(context: Context) {
+    private val client = OkHttpClient
+        .Builder()
+        .addInterceptor(
+            AccessTokenInterceptor(context)
+        )
+        .build()
+
+    data class StoreBody(
+        val name: String,
+        val description: String?,
+        val weight: String,
+        val buyPrice: Double,
+        val sellPrice: Double,
+        val stock: Double,
+        val barcode: String?,
+        val categoryId: String?,
+        var pictureId: String?
+    )
+
+    data class UpdateBody (
+        val name: String,
+        val description: String?,
+        val weight: String,
+        val buyPrice: Double,
+        val sellPrice: Double,
+        val stock: Double,
+        val barcode: String,
+        val categoryId: String?,
+        val pictureId: String?
+    )
+
+    suspend fun store(body: StoreBody): HttpState<Product> {
+        val request = ApiRequest
+            .json(
+                HttpMethod.POST,
+                "/products",
+                body
+            )
+            .build()
+
+        return HttpRequest
+            .create<Product>(
+                call = client.newCall(request),
+            )
+            .execute()
+    }
+
+    suspend fun update(productId: String, body: UpdateBody): HttpState<Product> {
+        val request = ApiRequest
+            .json(
+                HttpMethod.PUT,
+                "/products/$productId",
+                body
+            )
+            .build()
+
+        return HttpRequest
+            .create<Product>(
+                call = client.newCall(request),
+            )
+            .execute()
+    }
+
+    suspend fun getAll(query: String? = null): HttpState<List<Product>> {
+        val request = ApiRequest
+            .json(
+                HttpMethod.GET,
+                "/products",
+                params = mapOf(
+                    "query" to query
+                )
+            )
+            .build()
+
+        return HttpRequest
+            .create<List<Product>>(
+                call = client.newCall(request),
+            )
+            .execute()
+    }
+
+    suspend fun get(productId: String): HttpState<Product> {
+        val request = ApiRequest
+            .json(
+                HttpMethod.GET,
+                "/products/$productId"
+            )
+            .build()
+
+        return HttpRequest
+            .create<Product>(
+                call = client.newCall(request),
+            )
+            .execute()
+    }
+
+    suspend fun delete(productId: String): HttpState<Product> {
+        val request = ApiRequest
+            .json(
+                HttpMethod.DELETE,
+                "/products/$productId"
+            )
+            .build()
+
+        return HttpRequest
+            .create<Product>(
+                call = client.newCall(request),
+            )
+            .execute()
+    }
+}
