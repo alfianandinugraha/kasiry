ac21576bc969c246e2788ea8b3e8be67adeaf704
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-01-07 14:08:09 +0700

    feat(frontend): create model Product

---
+++ frontend/app/src/main/java/com/kasiry/app/models/data/Product.kt
@@ -0,0 +1,19 @@
+package com.kasiry.app.models.data
+
+data class Product(
+    val productId: String,
+    val name: String,
+    val weight: String,
+    val buyPrice: Double,
+    val sellPrice: Double,
+    val stock: Double,
+    val barcode: String,
+    val category: Category,
+    val company: Company,
+    val picture: Asset,
+) {
+    override fun toString(): String {
+        return "Product(productId='$productId', name='$name', weight='$weight', buyPrice=$buyPrice, sellPrice=$sellPrice, stock=$stock, barcode='$barcode', category=$category, company=$company, picture=$picture)"
+    }
+}
+
