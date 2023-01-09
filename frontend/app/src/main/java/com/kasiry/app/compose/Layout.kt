9b7b104ac0a339e34db3419f4486747a49e67041
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2023-01-09 23:42:01 +0700

    fix(frontend): fix background color in Layout

--- frontend/app/src/main/java/com/kasiry/app/compose/Layout.kt
+++ frontend/app/src/main/java/com/kasiry/app/compose/Layout.kt
@@ -1,5 +1,6 @@
 package com.kasiry.app.compose
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +10,7 @@ import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -21,6 +23,7 @@ fun Layout(
     Box {
         LazyColumn(
             modifier = Modifier
+                .background(Color.White)
                 .fillMaxWidth()
                 .fillMaxHeight()
         ) {
